669bcf82da45afcb3673d2c2426c8542473d8848
fanfan <fatfan1020@gmail.com>
2021-04-30 16:01:22 +0800

    feat: add swiper recommendList

---
+++ src/components/Slider/index.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import { SliderContainer } from "./style";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
+import "swiper/swiper.min.css";
+import "swiper/components/navigation/navigation.min.css";
+
+SwiperCore.use([Navigation, Pagination, Autoplay]);
+
+function Slider(props) {
+  const { bannerList } = props;
+  return (
+    <SliderContainer>
+      <div className="before" />
+      <Swiper autoplay>
+        {bannerList.map((slider, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <div className="slider-nav">
+                <img
+                  src={slider.imageUrl}
+                  width="100%"
+                  height="100%"
+                  alt="推荐"
+                />
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </SliderContainer>
+  );
+}
+
+export default React.memo(Slider);
