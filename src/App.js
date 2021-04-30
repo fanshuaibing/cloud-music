5d31f27349de792e5767e12c5d868296f67944e2
Shuaibing Fan <shuaibingfan@westwin.com>
2021-04-30 14:32:30 +0800

    feat:  add redux store

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import { GlobalStyle } from "./style";
 import { IconStyle } from "./assets/iconfont/iconfont";
 import { renderRoutes } from "react-router-config";
 import routes from "./routes";
+import store from "./store/index";
+
 function App() {
   return (
-    <HashRouter>
-      <div className="App">
-        <GlobalStyle />
-        <IconStyle />
-        {renderRoutes(routes)}
-      </div>
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <div className="App">
+          <GlobalStyle />
+          <IconStyle />
+          {renderRoutes(routes)}
+        </div>
+      </HashRouter>
+    </Provider>
   );
 }
 
